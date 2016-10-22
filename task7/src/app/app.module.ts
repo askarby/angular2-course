515eed403dc8d0540ba53c9633447457da781daa
Anders Skarby <abs@inno-tech.dk>
2016-10-22 17:55:40 +0200

    Added solution for task 7, and fixed bad error in task 6 solution... becoming clever, now

---
+++ task7/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
+import { ArticleTableComponent } from './article-table/article-table.component';
+import { RecieptComponent } from './reciept/reciept.component';
+import { CustomerService } from './shared/customer.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerFormComponent,
+    ArticleTableComponent,
+    RecieptComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [
+    CustomerService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
